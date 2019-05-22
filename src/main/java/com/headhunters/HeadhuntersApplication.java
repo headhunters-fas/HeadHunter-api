966d1a816243dfa976a9aefd0e6199e31e1a448e
diegosalas27 <dominicsc2hs@gmail.com>
2019-05-22 12:25:01 -0500

    added backend skeleton

---
+++ src/main/java/com/headhunters/HeadhuntersApplication.java
@@ -0,0 +1,13 @@
+package com.headhunters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadhuntersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeadhuntersApplication.class, args);
+    }
+
+}
